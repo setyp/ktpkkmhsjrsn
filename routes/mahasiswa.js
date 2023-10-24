0e0b68467861aa06cded854b62a4bfd11ea73027
setyp <river60cbc@gmail.com>
2023-10-24 15:15:37 +0700

    sampai23

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -29,7 +29,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({storage: storage, fileFilter: fileFilter})
 
 router.get('/mahasiswa', function (req, res){
-    connection.query('SELECT a.nama, b.nama_jurusan AS jurusan, a.gambar, a.swa_foto FROM mahasiswa a JOIN jurusan b ON b.id_j = a.id_jurusan ORDER BY a.id_m DESC;', function(err, rows){
+    connection.query('SELECT a.id_m,a.nama,a.nrp, b.nama_jurusan AS jurusan, a.gambar, a.swa_foto FROM mahasiswa a JOIN jurusan b ON b.id_j = a.id_jurusan ORDER BY a.id_m DESC;', function(err, rows){
         if(err){
             return res.status(500).json({
                 status: false,
@@ -105,76 +105,102 @@ router.get('/:id', function (req, res) {
     });
 });
 
-router.patch('/update/:id', upload.fields([{ name: 'gambar', maxCount: 1 },{ name: 'swa_foto', maxCount: 1 }]), [
-    // Validation using Express-validator
-    body('nama').notEmpty(),
-    body('nrp').notEmpty(),
-    body('id_jurusan').notEmpty()
-  ], (req, res) => {
+router.patch(
+  "/update/:id",
+  upload.fields([
+    { name: "gambar", maxCount: 1 },
+    { name: "swa_foto", maxCount: 1 },
+  ]),
+  [
+    body("nama").notEmpty(),
+    body("nrp").notEmpty(),
+    body("id_jurusan").notEmpty(),
+  ],
+  (req, res) => {
     const errors = validationResult(req);
-    
     if (!errors.isEmpty()) {
       return res.status(422).json({
-        errors: errors.array()
+        errors: errors.array(),
       });
     }
-    
+
     const id = req.params.id;
-    let gambar = req.files['gambar'] ? req.files['gambar'][0].filename : null;
-    let swa_foto = req.files['swa_foto'] ? req.files['swa_foto'][0].filename : null;
-    
-    connection.query(`SELECT * FROM mahasiswa WHERE id_m = ${id}`, function (err, rows) {
-      if (err) {
-        return res.status(500).json({
-          status: false,
-          message: 'Server Error',
-        });
-      }
-      if (rows.length === 0) {
-        return res.status(404).json({
-          status: false,
-          message: 'Not Found',
-        });
-      }
-      
-      const namaFileLama = rows[0].gambar;
-      const swa_fotoLama = rows[0].swa_foto;
-      
-      // Hapus file lama jika ada
-      if (namaFileLama && gambar) {
-        const pathFileLama = path.join(__dirname, '../public/images', namaFileLama);
-        fs.unlinkSync(pathFileLama);
-      }
-      
-      if (swa_fotoLama && swa_foto) {
-        const pathSwaFotoLama = path.join(__dirname, '../public/images', swa_fotoLama);
-        fs.unlinkSync(pathSwaFotoLama);
-      }
-      
-      let Data = {
-        nama: req.body.nama,
-        nrp: req.body.nrp,
-        id_jurusan: req.body.id_jurusan,
-        gambar: gambar,
-        swa_foto: swa_foto
-      };
-      
-      connection.query(`UPDATE mahasiswa SET ? WHERE id_m = ?`, [Data, id], function (err, rows) {
+
+    const gambar = req.files["gambar"] ? req.files["gambar"][0].filename : null;
+    const swa_foto = req.files["swa_foto"]
+      ? req.files["swa_foto"][0].filename
+      : null;
+
+    connection.query(
+      `SELECT * FROM mahasiswa WHERE id_m = ${id}`,
+      function (err, rows) {
         if (err) {
           return res.status(500).json({
             status: false,
-            message: 'Server Error',
-          });
-        } else {
-          return res.status(200).json({
-            status: true,
-            message: 'Update Success..!'
+            message: "Server Error",
           });
         }
-      });
-    });
-  });
-  
+        if (rows.length === 0) {
+          return res.status(404).json({
+            status: false,
+            message: "Not Found",
+          });
+        }
+
+        const gambarLama = rows[0].gambar;
+        const swa_fotoLama = rows[0].swa_foto;
+
+        if (gambarLama && gambar) {
+          const pathGambar = path.join(
+            __dirname,
+            "../public/images",
+            gambarLama
+          );
+          fs.unlinkSync(pathGambar);
+        }
+        if (swa_fotoLama && gambar) {
+          const pathSwa = path.join(
+            __dirname,
+            "../public/images",
+            swa_fotoLama
+          );
+          fs.unlinkSync(pathSwa);
+        }
+
+        let Data = {
+          nama: req.body.nama,
+          nrp: req.body.nrp,
+          id_jurusan: req.body.id_jurusan,
+        };
+
+        if (gambar) {
+          Data.gambar = gambar;
+        }
+        if (swa_foto) {
+          Data.swa_foto = swa_foto;
+        }
+
+        connection.query(
+          `UPDATE mahasiswa SET ? WHERE id_m = ${id}`,
+          Data,
+          function (err, result) {
+            if (err) {
+              return res.status(500).json({
+                status: false,
+                message: "Server Error",
+              });
+            } else {
+              return res.status(200).json({
+                status: true,
+                message: "Update Sukses..!",
+              });
+            }
+          }
+        );
+      }
+    );
+  }
+);
 
   router.delete('/delete/:id', (req, res) => {
     const id = req.params.id;
